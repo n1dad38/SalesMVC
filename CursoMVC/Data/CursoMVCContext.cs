5cb3901f16f29a81bb53b6fd7e4f3d1db0856025
Nilton <ni.flavio@hotmail.com>
2022-05-09 20:58:31 -0300

    Departments via Scaffolded

---
+++ CursoMVC/Data/CursoMVCContext.cs
@@ -0,0 +1,20 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CursoMVC.Models;
+
+namespace CursoMVC.Data
+{
+    public class CursoMVCContext : DbContext
+    {
+        public CursoMVCContext (DbContextOptions<CursoMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CursoMVC.Models.Department> Department { get; set; }
+    }
+}
--- CursoMVC/Program.cs
+++ CursoMVC/Program.cs
@@ -1,4 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using CursoMVC.Data;
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<CursoMVCContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("CursoMVCContext") ?? throw new InvalidOperationException("Connection string 'CursoMVCContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
